5ed421cebe98ff1087eac8c3994bee9954f741a1
jubo445 <juboraj335.ja@gmail.com>
2024-09-07 17:44:15 +0600

    Innitial added

--- app/src/main/java/com/example/fragment/MainActivity.kt
+++ app/src/main/java/com/example/fragment/MainActivity.kt
@@ -5,16 +5,52 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.fragment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    lateinit var appBarConfiguration: AppBarConfiguration
+    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolBar.root)
+
+        val drawerLayout = binding.drawerLayout
+        val navView = binding.navigationView
+
+        navController = findNavController(R.id.fragmentContainerView)
+
+        appBarConfiguration = AppBarConfiguration(
+
+            setOf(
+                R.id.homeFragment,
+                R.id.loginFragment,
+                R.id.userFragment
+
+            ), drawerLayout
+
+
+        )
+
+        setupActionBarWithNavController(navController, drawerLayout)
+        navView.setupWithNavController(navController)
+
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
 }
